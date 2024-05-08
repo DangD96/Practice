0d7578f7afe0413e1725e18d426322fdced1dc8e
DangD96 <35606061+DangD96@user.noreply.github.com>
2024-05-08 18:59:54 -0500

    update

---
+++ src/main/java/GeneralPractice/Strings/Main.java
@@ -0,0 +1,20 @@
+package GeneralPractice.Strings;
+
+import java.util.Arrays;
+
+public class Main {
+    final static String DIR = System.getProperty("user.dir");
+
+    public static void main(String[] args) {
+        System.out.println(DIR);
+
+        String[] results = DIR.split("\\\\"); // regex needs double escape. This is for "\"
+        System.out.println(results[results.length-2]);
+        String[] test = DIR.split(results[results.length-2]);
+        System.out.println(Arrays.toString(test));
+
+
+
+
+    }
+}
