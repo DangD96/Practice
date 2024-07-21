5b4a3fafd18412c155c99e36f0ac500c96aa5bfc
DangD96 <35606061+DangD96@user.noreply.github.com>
2024-07-20 20:54:05 -0500

    update

--- src/main/java/GeneralPractice/DateTime/Main.java
+++ src/main/java/GeneralPractice/DateTime/Main.java
@@ -11,12 +11,28 @@ public class Main {
         Instant inst1 = myObj.toInstant(ZoneOffset.ofHours(0));
         System.out.println(inst1);
 
-        LocalDateTime futureDateTime = LocalDateTime.parse("2024-07-20T20:30:55");
+        LocalDateTime futureDateTime = LocalDateTime.parse("2024-07-20T21:00:55");
         Instant inst2 = futureDateTime.toInstant(ZoneOffset.ofHours(0));
         System.out.println(inst2);
 
         Duration duration = Duration.between(inst1, inst2);
         System.out.println(duration);
         System.out.println("Duration: " + duration.toMinutesPart() + " - " + duration.toSecondsPart() + " - " + duration.toNanosPart());
+
+        System.out.println(Math.round(1.5));
+        System.out.println(Math.round(2.3));
+        System.out.println(Math.round(0.2));
+
+        int minutes = duration.toMinutesPart();
+        double minutesFraction = (double) duration.toSecondsPart() / 60;
+        String runTimeStr = String.valueOf(minutes + minutesFraction);
+        int runTimeDouble = (int) Math.round(Double.parseDouble(runTimeStr));
+        System.out.println(runTimeDouble);
+
+
+        if (runTimeDouble < 1) {
+            System.out.println("1");
+        }
+        System.out.println(runTimeDouble);
     }
 }
