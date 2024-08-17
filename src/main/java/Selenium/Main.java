42fd77e6b140a5f9be62ea5255bac2d1ce6be254
DangD96 <35606061+DangD96@user.noreply.github.com>
2024-08-17 00:01:15 -0500

    update

--- src/main/java/Selenium/Main.java
+++ src/main/java/Selenium/Main.java
@@ -12,20 +12,20 @@ public class Main {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
+
+        // With By
         By googleSearch = By.cssSelector("[value='Google Search']");
         WebElement googleSearchBtn = driver.findElement(googleSearch);
-        System.out.println(VM.current().addressOf(googleSearch)); // same address
         System.out.println(VM.current().addressOf(googleSearch)); // same address
         googleSearchBtn = driver.findElement(googleSearch);
         System.out.println(VM.current().addressOf(googleSearchBtn)); // different address
 
+        System.out.println("----------");
 
         // With page factory
         GooglePage g = new GooglePage(driver);
         System.out.println(VM.current().addressOf(g.SEARCH_BTN)); // same address
-        System.out.println(VM.current().addressOf(g.SEARCH_BTN)); // same address
-        g = new GooglePage(driver);
-        System.out.println(VM.current().addressOf(g.SEARCH_BTN)); // // different address
+        System.out.println(VM.current().addressOf(g.SEARCH_BTN)); // // same address
         driver.quit();
     }
 }
