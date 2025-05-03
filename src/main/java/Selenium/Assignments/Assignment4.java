c5468203af9377f2e0b504305beae6f16fada07f
DangD96 <35606061+DangD96@user.noreply.github.com>
2025-05-02 23:35:21 -0500

    add

---
+++ src/main/java/Selenium/Assignments/Assignment4.java
@@ -0,0 +1,58 @@
+package Selenium.Assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Assignment4 {
+    public static void main (String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        try {
+            driver.get("https://the-internet.herokuapp.com/");
+            waitForElement(driver, By.linkText("Multiple Windows")).click();
+
+            // Open up child window
+            waitForElement(driver, By.linkText("Click Here")).click();
+
+            // Get window handles
+            Set<String> windowHandles = driver.getWindowHandles();
+            Iterator<String> it = windowHandles.iterator();
+            String parentWindowID = it.next();
+            String childWindowID = it.next();
+
+            // Switch to child window and print out text
+            driver.switchTo().window(childWindowID);
+            printHeaderContents(driver);
+
+            // Switch back to parent window and print out text
+            driver.switchTo().window(parentWindowID);
+            printHeaderContents(driver);
+
+        } catch(Exception e) {
+            System.out.println("Error: " + Arrays.toString(e.getStackTrace()));
+
+        } finally {
+            driver.quit();
+        }
+    }
+
+    public static void printHeaderContents(WebDriver driver) {
+        String contents = waitForElement(driver, By.tagName("h3")).getText();
+        System.out.println(contents);
+    }
+    public static WebElement waitForElement(WebDriver driver, By byMethod) {
+        // https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/support/ui/ExpectedConditions.html
+        return new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(byMethod));
+    }
+}
---
+++ src/main/java/Selenium/Assignments/Assignment7.java
@@ -0,0 +1,64 @@
+package Selenium.Assignments;
+
+import helper.Wait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.List;
+
+/*
+1. Go to site: https://rahulshettyacademy.com/AutomationPractice/
+2. Find the example web table and scroll to it
+3. Print # of rows present in table
+4. Print # of columns present in table
+5. Print entire 2nd row
+*/
+public class Assignment7 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        try {
+            driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollBy(0,450)");
+            WebElement webTable = Wait.forElementVisible(driver, By.id("product"));
+
+            // Other way to scroll using Actions API
+            /*
+            -If element is off-screen
+            -scrollToElement seems to stop when element becomes visible (i.e. it didn't scroll to the table bc it was already visible)
+
+            new Actions(driver)
+                    .scrollToElement(webTable)
+                    .perform();
+            Didn't need build() method before perform()
+            https://www.selenium.dev/documentation/webdriver/actions_api/
+            */
+
+            int numRows = webTable.findElements(By.tagName("tr")).size();
+            System.out.printf("Number of rows: %s\n", numRows);
+
+            int numCols = webTable.findElement(By.cssSelector("tr")).findElements(By.tagName("th")).size();
+            System.out.printf("Number of columns: %s\n", numCols);
+
+            // Print contents of 2nd row (excluding the table header)
+            WebElement secondDataRow = webTable.findElements(By.tagName("tr")).get(2);
+            List<WebElement> secondDataRowColumns = secondDataRow.findElements(By.tagName("td"));
+            for (WebElement column : secondDataRowColumns) {
+                System.out.println(column.getText());
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            driver.quit();
+        }
+    }
+}
